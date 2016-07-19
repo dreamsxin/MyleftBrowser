16130b619c1b4c75eb339ff84221a3f80de7c0e0
Dreamsxin <dreamsxin@126.com>
2016-07-19 17:58:01 +0800

    Open url

--- app/src/main/java/org/myleftstudio/browser/MainActivity.java
+++ app/src/main/java/org/myleftstudio/browser/MainActivity.java
@@ -2,12 +2,18 @@ package org.myleftstudio.browser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import org.xwalk.core.XWalkView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private XWalkView mXWalkView;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        mXWalkView = (XWalkView) findViewById(R.id.xwalkView);
+        mXWalkView.load("http://www.myleft.org/", null);
     }
 }
